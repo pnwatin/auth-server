300d6eb8e5b542f3abcfc9507b10afc3863edc1e
Nicolas <pnwa@icloud.com>
2024-03-22 00:39:01 +0100

    chore: add basic init_db script

---
+++ scripts/init_db.sh
@@ -0,0 +1,54 @@
+#!/usr/bin/env bash
+
+set -eo pipefail
+
+if ! [ -x "$(command -v psql)" ]; then
+	echo >&2 "Error: psql is not installed."
+	exit 1
+fi
+
+if ! [ -x "$(command -v sqlx)" ]; then
+	echo >&2 "Error: sqlx is not installed."
+	echo >&2 "Use:"
+	echo >&2 "    cargo install --version='~0.7' sqlx-cli --no-default-features --features rustls,postgres"
+	echo >&2 "to install it."
+	exit 1
+fi
+
+DB_USER="${POSTGRES_USER:=postgres}"
+DB_PASSWORD="${POSTGRES_PASSWORD:=password}"
+DB_NAME="${POSTGRES_DB:=matoscout}"
+DB_PORT="${POSTGRES_PORT:=5432}"
+DB_HOST="${POSTGRES_HOST:=localhost}"
+
+CONTAINER_NAME="${CONTAINER_NAME:=matoscout}"
+
+if [[ -z "${SKIP_DOCKER}" ]]; then
+	if docker container inspect "$CONTAINER_NAME" &>/dev/null; then
+		echo >&2 "The '$CONTAINER_NAME' container is already running."
+		echo >&2 "To stop it, use: docker container stop $CONTAINER_NAME"
+		exit 1
+	fi
+
+	docker run \
+		-e POSTGRES_USER="${DB_USER}" \
+		-e POSTGRES_PASSWORD="${DB_PASSWORD}" \
+		-e POSTGRES_DB="${DB_NAME}" \
+		-p "${DB_PORT}":5432 \
+		-d \
+		--name "$CONTAINER_NAME" \
+		postgres -N 1000
+fi
+
+until PGPASSWORD="${DB_PASSWORD}" psql -h "${DB_HOST}" -U "${DB_USER}" -p "${DB_PORT}" -d "postgres" -c '\q'; do
+	>&2 echo "Postgres is still unavailable - waiting"
+	sleep 1
+done
+
+>&2 echo "Postgres is up and running on port ${DB_PORT} - running migrations now..."
+
+export DATABASE_URL=postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}
+sqlx database create
+sqlx migrate run
+
+>&2 echo "Postgres has been successfully migrated!"
