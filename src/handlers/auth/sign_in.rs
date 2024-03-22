189a599cbcd80772be7e95fcf480a070de00c55f
Nicolas <pnwa@icloud.com>
2024-03-22 23:14:23 +0100

    chore: add json extractor to sign-in handler

--- src/handlers/auth/sign_in.rs
+++ src/handlers/auth/sign_in.rs
@@ -1,5 +1,15 @@
-use axum::{http::StatusCode, response::IntoResponse};
+use axum::{http::StatusCode, response::IntoResponse, Json};
+use secrecy::Secret;
+use serde::Deserialize;
 
-pub async fn sign_in_handler() -> impl IntoResponse {
+use crate::domain::Email;
+
+pub async fn sign_in_handler(Json(_payload): Json<SignInPayload>) -> impl IntoResponse {
     StatusCode::OK
+}
+
+#[derive(Deserialize)]
+pub struct SignInPayload {
+    pub email: Email,
+    pub password: Secret<String>,
 }
---
+++ tests/api/auth/sign_in.rs
@@ -0,0 +1,34 @@
+use serde_json::json;
+
+use crate::helpers::TestApplication;
+
+#[tokio::test]
+async fn sign_in() {
+    let app = TestApplication::spawn().await;
+
+    let test_cases = vec![
+        (json!({}), "missing data"),
+        (json!({"password": "password"}), "missing email"),
+        (json!({"email": "test@domain.com"}), "missing password"),
+        (
+            json!({"email": "testwrong@@gmail.com", "password": "password"}),
+            "invalid email",
+        ),
+    ];
+
+    for (invalid_body, error_message) in test_cases {
+        let response = app
+            .post("/auth/sign-in")
+            .json(&invalid_body)
+            .send()
+            .await
+            .expect("Failed to execute request.");
+
+        assert_eq!(
+            422,
+            response.status().as_u16(),
+            "/auth/sign-in did not return 422 when body was {}",
+            error_message
+        );
+    }
+}
