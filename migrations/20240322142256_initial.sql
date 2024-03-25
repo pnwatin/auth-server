cc9d81de40e4866b4fe9c16a7d844a7bd20672c3
Nicolas <pnwa@icloud.com>
2024-03-25 23:57:34 +0100

    feat: add refresh_tokens table

--- migrations/20240322142256_initial.sql
+++ migrations/20240322142256_initial.sql
@@ -1,7 +1,19 @@
 DROP TABLE IF EXISTS users;
+-- DROP TABLE IF EXISTS refresh_tokens;
 
 create table users(
   id UUID PRIMARY KEY NOT NULL,
   email TEXT NOT NULL UNIQUE,
   password_hash TEXT NOT NULL
 );
+
+CREATE TABLE refresh_tokens(
+  id UUID PRIMARY KEY NOT NULL,
+  user_id UUID NOT NULL
+    REFERENCES users (id),
+  jit UUID NOT NULL UNIQUE,
+  family UUID NOT NULL UNIQUE,
+  expires_at TIMESTAMPTZ NOT NULL,
+  created_at TIMESTAMPTZ NOT NULL
+);
+
